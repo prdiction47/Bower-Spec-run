51da2745f96ac8b05fe0f9a6f6f0c71e8fc502c4
prdiction47 <52804197+prdiction47@users.noreply.github.com>
2019-07-20 10:49:42 -0700

    Initial Commit

---
+++ specs/example.spec.js
@@ -0,0 +1,16 @@
+/**
+ * Module module
+ *
+ * @author whocares
+ * @date 2016-12-22
+ */
+
+define(['modules/module'], function (module) {
+  'use strict';
+
+  // Test suite module
+  describe('module', function () {
+
+  });
+
+});
